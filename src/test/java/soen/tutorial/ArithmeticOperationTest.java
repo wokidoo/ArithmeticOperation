034387fa1d7c6731614f58d97251e2a029bb73b9
Charles Partous <charles.partous@gmail.com>
2025-04-02 16:49:06 -0400

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -14,7 +14,8 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		//assertEquals(expected, actual);
+		fail("must fail");
 
 	}
     // @Test
